b7b5f11731038fdc4e1d268481f07ee6b2a7252a
Max <gameeter@gmail.com>
2022-08-18 01:29:55 +0300

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
 const Home = () => {
     return (
@@ -8,7 +9,26 @@ const Home = () => {
                 <meta name="description" content="Сайт юридической помощи"/>
                 <meta charSet="utf-8"/>
             </Head>
-            <h1>Главная</h1>
+            <div className={styles.wrapper}>
+                <div className={styles.wrapper__back}>
+                    <header className={styles.header}>
+                        <h1>ЮРИСТЫ</h1>
+                        <button className={styles.button}>Записаться на консультацию</button>
+                    </header>
+                    <nav className={styles.navigation}>
+                        <ul className={styles.navigation__list}>
+                            <li><a className={styles.navigation__item} href="#">Главная</a></li>
+                            <li><a className={styles.navigation__item} href="#">Контакты</a></li>
+                            <li><a className={styles.navigation__item} href="#">О нас</a></li>
+                        </ul>
+                        <div className={styles.contacts}>
+                            <a className={styles.contacts__item} href="tel: +74951234567">+7 (495) 123-45-67</a>
+                            <a href="mailto: example@mail.ru">example@mail.ru</a>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+
         </>
     )
 }
