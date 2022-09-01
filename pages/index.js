6ecd9797d112b3340f270a64592455b3a6f76f51
Max <gameeter@gmail.com>
2022-09-01 20:04:15 +0300

    много клевых правок

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import man from "../public/img/man.png"
+import Image from "next/image";
 
 const Home = () => {
     return (
@@ -9,26 +11,38 @@ const Home = () => {
                 <meta name="description" content="Сайт юридической помощи"/>
                 <meta charSet="utf-8"/>
             </Head>
-            <div className={styles.wrapper}>
-                <div className={styles.wrapper__back}>
-                    <header className={styles.header}>
-                        <h1>ЮРИСТЫ</h1>
-                        <button className={styles.button}>Записаться на консультацию</button>
-                    </header>
-                    <nav className={styles.navigation}>
-                        <ul className={styles.navigation__list}>
-                            <li><a className={styles.navigation__item} href="#">Главная</a></li>
-                            <li><a className={styles.navigation__item} href="#">Контакты</a></li>
-                            <li><a className={styles.navigation__item} href="#">О нас</a></li>
-                        </ul>
-                        <div className={styles.contacts}>
-                            <a className={styles.contacts__item} href="tel: +74951234567">+7 (495) 123-45-67</a>
-                            <a href="mailto: example@mail.ru">example@mail.ru</a>
+            <div className={styles.container}>
+                <header className={styles.header}>
+                    <h1>ЮРИСТЫ</h1>
+                    <button className={styles.button}>Записаться на консультацию</button>
+                </header>
+                <nav className={styles.nav}>
+                    <ul>
+                        <li><a href="/">Главная</a></li>
+                        <li><a href="#">Контакты</a></li>
+                        <li><a href="#">О нас</a></li>
+                    </ul>
+                    <div className={styles.contacts}>
+                        <Image src="/img/call.svg" width={40} height={40} alt="call"/>
+                        <a href="tel: +74951234567">+7 (495) 123-45-67</a>
+                        <Image src="/img/mail.svg" width={40} height={40} alt="call"/>
+                        <a href="mailto: example@mail.ru">example@mail.ru</a>
+                    </div>
+                </nav>
+                <main>
+                    <section className={styles.block}>
+                        <div>
+                            <Image src="/img/херня.svg" width={95} height={44} />
+                            <h2>We're Group Of Certified Law Professionals</h2>
+                            <p>we have helped countless maritime workers and their families go up against the largest offshore companies and win.</p>
+                            <button>Get In Touch</button>
                         </div>
-                    </nav>
-                </div>
+                        <div className={styles.image}>
+                            <Image src={man} width={672} height={708} alt="alt"/>
+                        </div>
+                    </section>
+                </main>
             </div>
-
         </>
     )
 }
